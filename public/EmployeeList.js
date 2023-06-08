886c9acafd884d62712820db63059dd0fe032c34
Your Name <anneserocki@gmail.comom>
2023-06-08 10:02:19 -0700

    V1.0.3

--- public/EmployeeList.js
+++ public/EmployeeList.js
@@ -3,7 +3,6 @@ import { Badge, Button, Table, NavBar, Nav, Col, Modal } from 'react-bootstrap';
 import EmployeeFilter from './EmployeeFilter.jsx';
 import EmployeeAdd from './EmployeeAdd.jsx';
 
-
 class EmployeeRow extends React.Component {
   constructor(props) {
     super(props);
@@ -25,4 +24,43 @@ class EmployeeRow extends React.Component {
     console.log('Employee deleted');
     
     // Hide the modal after deletion
-    t
+    this.toggleModal();
+  }
+
+  render() {
+    return (
+      <tr>
+        <td>{this.props.employee._id}</td>        
+        <td>
+          <Button variant="danger" onClick={this.toggleModal}>
+            Delete
+          </Button>
+          <Modal show={this.state.modalVisible} onHide={this.toggleModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>Confirm Delete</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Are you sure you want to delete {this.props.employee.name}?
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.toggleModal}>
+                Cancel
+              </Button>
+              <Button variant="danger" onClick={this.deleteEmployee}>
+                Yes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </td>
+      </tr>
+    );
+  }
+}
+
+function EmployeeTable(props) {
+  // ...
+}
+
+export default class EmployeeList extends React.Component {
+  // ...
+}
